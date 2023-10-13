80e4b3cef44af3c2071991a3c8832746b224cb44
sriha <sriha@sriharika>
2023-10-12 21:26:39 -0500

    Adding fetch-products api

--- Backend/app.py
+++ Backend/app.py
@@ -22,6 +22,7 @@ cursor = db.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Users (
         Id INT AUTO_INCREMENT PRIMARY KEY,
+        role ENUM('Seller', 'Buyer', 'Admin'),
         username VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
@@ -31,19 +32,66 @@ cursor.execute('''
     )
 ''')
 # Sample_data = {
+#     'role': 'Seller',
 #     'username': 'BHARAT',
 #     'email': 'bARATH@sl.edu',
 #     'password': 'password12345',
 #     'first_name': 'BHARAT',
 #     'last_name': 'User'
 # }
-# # creating dummy record to get hash password entry in db
+# # # creating dummy record to get hash password entry in db
 # hashed_password = hashlib.sha256(Sample_data['password'].encode()).hexdigest()
 # cursor.execute('''
-#     INSERT INTO Users (username, email, password, first_name, last_name)
+#     INSERT INTO Users (role,username, email, password, first_name, last_name)
+#     VALUES (%s, %s, %s, %s, %s, %s)
+# ''', (Sample_data['role'], Sample_data['username'], Sample_data['email'], hashed_password, Sample_data['first_name'], Sample_data['last_name']))
+# db.commit()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Category (
+        category_Id INT AUTO_INCREMENT PRIMARY KEY,
+        category_name VARCHAR(255)
+    )
+    AUTO_INCREMENT = 101
+''')
+
+# Create the "products" table if it doesn't exist
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products (
+        product_id INT AUTO_INCREMENT PRIMARY KEY,
+        product_name VARCHAR(255),
+        product_desc TEXT,
+        price DECIMAL(10, 2),
+        image LONGBLOB,
+        category_id INT,
+        FOREIGN KEY (category_id) REFERENCES Category(category_Id)
+    )
+''')
+
+
+# Sample_data1={'category_name': 'Electronics'
+# }
+# cursor.execute('''
+#     INSERT INTO Category (category_name)
+#     VALUES (%s)
+# ''', (Sample_data1['category_name'],))
+# db.commit()
+
+# Sample_product_data = {
+#     'product_name': 'Laptop',
+#     'product_desc': 'High-performance laptop with the latest specs.',
+#     'price': 999.99,
+#     'image': b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x01\xf4...',
+#     'category_id': 101  # Adjust the category_id to match the category you created
+# }
+
+# cursor.execute('''
+#     INSERT INTO Products (product_name, product_desc, price, image, category_id)
 #     VALUES (%s, %s, %s, %s, %s)
-# ''', (Sample_data['username'], Sample_data['email'], hashed_password, Sample_data['first_name'], Sample_data['last_name']))
-db.commit()
+# ''', (Sample_product_data['product_name'], Sample_product_data['product_desc'], Sample_product_data['price'], Sample_product_data['image'], Sample_product_data['category_id']))
+# db.commit()
+
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -62,6 +110,26 @@ def login():
         return jsonify({'message': 'Login successfullly'})
     else:
         return jsonify({'message': 'Invalid credentials are given'}), 401
+@app.route('/fetch-products', methods=['GET'])
+def fetch_products():
+    # Fetch all products based on category
+    category_id = request.args.get('category_id')
 
+    cursor.execute('SELECT * FROM Products WHERE category_id = %s', (category_id,))
+    products = cursor.fetchall()
+
+    product_list = []
+    for product in products:
+        product_dict = {
+            'product_id': product[0],
+            'product_name': product[1],
+            'product_desc': product[2],
+            'price': float(product[3]),
+            'category_id': product[5],
+            'image':'.\lappy.jpg'
+        }
+        product_list.append(product_dict)
+
+    return jsonify({'products': product_list})
 if __name__ == '__main__':
     app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Revive_Gallery
+To run the backend application, please install the below packages and goto the backend directory and give the command to run: python -m flask --app .\app.py run
+pip install mysql-connector-python
+pip install bcrypt
+pip install flask
+Use postman to check the APi's
+below are the methods we used so far:
