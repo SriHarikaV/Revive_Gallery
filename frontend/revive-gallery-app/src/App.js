7606db3d805e254b6fd2f7c9aa8d74fe760ab7dc
SaiKiran0407 <saikiranreddy.beeram@slu.edu>
2023-10-22 16:04:38 -0500

    MP-16#developed product details page to display product details

--- frontend/revive-gallery-app/src/App.js
+++ frontend/revive-gallery-app/src/App.js
@@ -2,6 +2,7 @@ import LoginForm from "./components/loginform";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegistrationForm from "./components/registrationform";
 import Marketplace from "./components/marketplace";
+import ProductDetails from "./components/productdetails";
 
 
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegistrationForm />} />
           <Route path="/mp" element={<Marketplace />} />
+          <Route path="/products/1" element={<ProductDetails />} />
       </Routes>   
     </Router>
     
