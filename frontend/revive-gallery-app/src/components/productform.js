203b48f17d04ae2387317f3ae0b764be58cd393c
anjaliputta <aputta2@slu.edu>
2023-10-23 12:52:03 -0500

    MP-15# updated product form to accept multiple categories and images uploads

--- frontend/revive-gallery-app/src/components/productform.js
+++ frontend/revive-gallery-app/src/components/productform.js
@@ -5,8 +5,9 @@ function ProductForm() {
   const [product, setProduct] = useState({
     name: '',
     description: '',
-    category: 'Select Category',
+    categories: [], // array to store selected categories
     price: '',
+    images: [] // array to store selected images
   });
 
   const handleInputChange = (e) => {
@@ -17,36 +18,56 @@ function ProductForm() {
     });
   };
 
+  const handleCategoryChange = (e) => {
+    const { name, options } = e.target;
+    const selectedCategories = Array.from(options).filter((option) => option.selected).map((option) => option.value);
+    setProduct({
+      ...product,
+      [name]: selectedCategories,
+    });
+  };
+
+  const handleImageChange = (e) => {
+    const selectedImages = Array.from(e.target.files);
+    setProduct({
+      ...product,
+      images: selectedImages,
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-        // Send the product data to the backend API
-        fetch("http://localhost:5000/product/create", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(product),
-        })
-            .then((response) => {
-                if (response.ok) {
-                    console.log("success")
-                    console.log('Form Data:', product);
-                } else {
-                    console.log("bad response");
-                }
-            })
-            .catch((error) => {
-                console.log("error");
-            });
-    
+    // Send the product data to the backend API
+    const formData = new FormData();
+    formData.append("name", product.name);
+    formData.append("description", product.description);
+    formData.append("price", product.price);
+    product.categories.forEach((category) => formData.append("categories", category));
+    product.images.forEach((image) => formData.append("images", image));
+
+    fetch("http://localhost:5000/product/create", {
+      method: "POST",
+      body: formData, 
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Success");
+          console.log('Form Data:', product);
+        } else {
+          console.log("Bad response");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   return (
     <div className="ProductForm">
       <h2>Add a New Product</h2>
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className="form-group">
           <label htmlFor="name">Product Name:</label>
           <input
             type="text"
@@ -54,32 +75,35 @@ function ProductForm() {
             name="name"
             value={product.name}
             onChange={handleInputChange}
+            className="form-control"
           />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="description">Description:</label>
           <textarea
             id="description"
             name="description"
             value={product.description}
             onChange={handleInputChange}
+            className="form-control"
           />
         </div>
-        <div>
-          <label htmlFor="category">Category:</label>
+        <div className="form-group">
+          <label htmlFor="categories">Categories:</label>
           <select
-            id="category"
-            name="category"
-            value={product.category}
-            onChange={handleInputChange}
+            id="categories"
+            name="categories"
+            multiple // Enable multiple selection
+            value={product.categories}
+            onChange={handleCategoryChange}
+            className="form-control"
           >
-            <option value="Select Category" disabled>Select Category</option>
             <option value="Electronics">Electronics</option>
             <option value="Clothing">Clothing</option>
             <option value="Books">Books</option>
           </select>
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="price">Price:</label>
           <input
             type="number"
@@ -87,10 +111,24 @@ function ProductForm() {
             name="price"
             value={product.price}
             onChange={handleInputChange}
+            className="form-control"
           />
         </div>
-        <div>
-          <button type="submit">Add Product</button>
+        <div className="form-group">
+          <label htmlFor="images">Product Images:</label>
+          <input
+            type="file"
+            id="images"
+            name="images"
+            multiple // Enable multiple file selection
+            onChange={handleImageChange}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Add Product
+          </button>
         </div>
       </form>
     </div>
