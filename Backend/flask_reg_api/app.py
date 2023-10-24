fb8804c6a5f50e7e319487aa147fb2831cab51d3
anjaliputta <aputta2@slu.edu>
2023-10-24 16:25:36 -0500

    testing circle ci pipelines

--- Backend/flask_reg_api/app.py
+++ Backend/flask_reg_api/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
+from flask_cors import CORS
+
 import hashlib
 from decouple import config
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['MYSQL_HOST'] = config('MYSQL_HOST')
 app.config['MYSQL_USER'] = config('MYSQL_USER')
