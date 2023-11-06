7158cf59007e4a7b06b219728894bc446a3bbc5e
anjaliputta <aputta2@slu.edu>
2023-11-06 16:50:32 -0600

    MP-34 designed nav bar with few links

--- frontend/revive-gallery-app/src/App.js
+++ frontend/revive-gallery-app/src/App.js
@@ -7,23 +7,27 @@ import ProductDetails from "./components/products/ProductDetails";
 import Homepage from "./components/home/HomePage";
 import { UserProvider } from './components/auth/UserContext';
 import PrivateRoute from './components/auth/PrivateRoute';
+import Navbar from './components/home/Navbar'; // Import the Navbar component
 
 function App() {
   return (
     <UserProvider>
       <Router>
-        <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegistrationForm />} />
-            <Route path="/products" element={<ProductsList />} />
-            <Route exact path='/products/details' element={<PrivateRoute/>}>
-              <Route exact path='/products/details' element={<ProductDetails/>}/>
-            </Route>
-            <Route exact path='/addproduct' element={<PrivateRoute/>}>
-              <Route exact path='/addproduct' element={<ProductForm/>}/>
-            </Route>
-        </Routes>   
+      <div>
+          <Navbar />
+          <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/register" element={<RegistrationForm />} />
+              <Route path="/products" element={<ProductsList />} />
+              <Route exact path='/products/details' element={<PrivateRoute/>}>
+                <Route exact path='/products/details' element={<ProductDetails/>}/>
+              </Route>
+              <Route exact path='/addproduct' element={<PrivateRoute/>}>
+                <Route exact path='/addproduct' element={<ProductForm/>}/>
+              </Route>
+          </Routes>  
+        </div> 
       </Router>
     </UserProvider>
   );
