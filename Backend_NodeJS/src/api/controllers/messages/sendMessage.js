2f40926dbd936a96c9b46a20910a077abd24488f
sriha <sriha@sriharika>
2023-11-09 20:42:29 -0600

    MP-37 Create Chat with Owner Button on Product Details Page

---
+++ Backend_NodeJS/src/api/controllers/messages/sendMessage.js
@@ -0,0 +1,40 @@
+const { ChatRoom } = require("../../../models/index.js");
+
+const sendMessage = async (req, res) => {
+  const { sender, chatId, message } = req.body;
+
+  try {
+    const chat = await ChatRoom.updateOne(
+      { _id: chatId },
+      { $push: { conversation: { senderId: sender, message } } }
+    );
+
+    if (chat.nModified === 0) {
+      // If no documents were modified, the chat room with the given ID doesn't exist
+      return res.status(404).json({ error: "Chat room not found." });
+    }
+
+    res.status(200).json({ sender: sender });
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).json({ error: "Internal server error." });
+  }
+};
+
+const sendMessageSocket = async (body) => {
+  const { sender, chatId, message } = body;
+  const chat = await ChatRoom.updateOne(
+    { _id: chatId },
+    { $push: { conversation: { senderId: sender, message } } }
+  );
+
+  if (chat.nModified === 0) {
+    // If no documents were modified, the chat room with the given ID doesn't exist
+    return false;
+  }
+
+  return true;
+};
+exports.sendMessage = sendMessage;
+exports.sendMessageSocket = sendMessageSocket;
